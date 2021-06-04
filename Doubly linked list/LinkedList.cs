96806b023bafd0b7d35b50d5d55b8bc4a0ad0323
Sergey <grozoff92@gmail.com>
2021-06-04 10:23:10 +0500

    Add project files.

---
+++ Doubly linked list/LinkedList.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Doubly_linked_list
+{
+    public class LinkedList : ILinkedList
+    {
+        private int count = 0;
+        private Node FirstNode;
+        private Node LastNode;
+
+        public int GetCount() => count;
+
+        public void AddNode(int value)
+        {
+            if (FirstNode == null)
+            {
+                FirstNode = LastNode = new Node() { Value = value };
+            }
+            else
+            {
+                var newItem = new Node() { Value = value, PrevNode = LastNode };
+                LastNode.NextNode = newItem;
+                LastNode = newItem;
+            }
+            count++;
+        }
+
+        public Node GetNodeByIndex(int nodeIndex)
+        {
+            Node node;
+
+            if (count - 1 >= nodeIndex)
+            {
+                node = FirstNode;
+                for (var i = 1; i <= nodeIndex; i++)
+                {
+                    node = node.NextNode;
+                }
+            }
+            else
+            {
+                node = LastNode;
+                for (var i = count - 1; i > nodeIndex; i--)
+                {
+                    node = node.PrevNode;
+                }
+            }
+            return node;
+        }
+
+        public void AddNodeAfter(Node node, int value)
+        {
+            var next = node.NextNode;
+            var newItem = new Node() { Value = value, NextNode = next, PrevNode = node };
+            node.NextNode = newItem;
+            if (next != null)
+            {
+                next.PrevNode = newItem;
+            }
+            count++;
+        }
+
+        public void RemoveNode(int index)
+        {
+            if (index == 0)
+            {
+                if (FirstNode.NextNode is { } node)
+                {
+                    FirstNode = node;
+                    node.PrevNode = null;
+                }
+                else
+                {
+                    FirstNode = LastNode = null;
+                }
+            }
+            else
+            {
+                var del = GetNodeByIndex(index);
+
+                if (del.NextNode != null)
+                {
+                    del.NextNode.PrevNode = del.PrevNode;
+                }
+                del.PrevNode.NextNode = del.NextNode;
+                del.NextNode = del.PrevNode = null;
+            }
+            count--;
+        }
+
+        public void RemoveNode(Node node)
+        {
+            if (FirstNode == node)
+            {
+                if (FirstNode == LastNode)
+                {
+                    FirstNode = LastNode = null;
+                }
+                else
+                {
+                    FirstNode = node.NextNode;
+                    node.NextNode = null;
+                    FirstNode.PrevNode = null;
+                }
+            }
+            else
+            {
+                if (node.NextNode != null)
+                {
+                    node.NextNode.PrevNode = node.PrevNode;
+                }
+                node.PrevNode.NextNode = node.NextNode;
+                node.NextNode = node.PrevNode = null;
+            }
+            count--;
+        }
+
+        public Node FindNode(int searchValue)
+        {
+            var current = FirstNode;
+            while (current != null)
+            {
+                if (current.Value == searchValue)
+                {
+                    return current;
+                }
+                current = current.NextNode;
+            }
+            return null;
+        }
+    }
+}
---
+++ Doubly linked list/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Doubly_linked_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new LinkedList();
+            list.AddNode(72);
+            list.AddNode(65);
+            list.AddNode(34);
+            list.AddNode(91);
+            var findedNode = list.FindNode(72);
+            var nodeByIndex = list.GetNodeByIndex(0);
+            var addAfter = list.FindNode(72);
+            list.AddNodeAfter(addAfter, 98);
+            findedNode = list.FindNode(98);
+            nodeByIndex = list.GetNodeByIndex(1);
+            var count = list.GetCount();
+        }
+    }
+}
